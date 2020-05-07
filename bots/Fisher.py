2b5864111b051cd348b5cc0beea82f44b941ec4f
Acurisu <acurisu@gmail.com>
2020-05-07 18:01:08 +0200

    [FIX] Adapted to refactoring

--- bots/Fisher.py
+++ bots/Fisher.py
@@ -53,7 +53,7 @@ class Bot:
 
     def spawn_object(self, packet):
         if packet.type_id == 102 and not self._active and calc_distance(self._client, packet) < self._range_bobber_detection:
-            self._terminal.log(f'[FISH] Found fishing bobber ({hex(packet.entity_id):s})')
+            self._terminal.console.log(f'[FISH] Found fishing bobber ({hex(packet.entity_id):s})')
             self._bobber = Bobber(packet.entity_id, packet.x, packet.y, packet.z)
             self._active = True
 
@@ -61,13 +61,13 @@ class Bot:
         if self._active:
             for entity_id in packet.entity_ids:
                 if entity_id == self._bobber.id:
-                    self._terminal.log(f'[FISH] Bobber got destroyed')
+                    self._terminal.console.log(f'[FISH] Bobber got destroyed')
                     self._active = False
                     threading.Timer(self._sleep, self._use_item).start()
 
     def sound_effect(self, packet):
         if self._active and packet.sound_id == 73 and calc_distance(self._bobber, packet.effect_position) < self._range_bobber_sound:
-            self._terminal.log(f'[FISH] Caught something')
+            self._terminal.console.log(f'[FISH] Caught something')
             self._active = False
             self._use_item()
             threading.Timer(self._sleep, self._use_item).start()
@@ -75,10 +75,10 @@ class Bot:
     # Custom methods
     def _fish(self):
         if not self._active:
-            self._terminal.log(f'[FISH] Started fishing')
+            self._terminal.console.log(f'[FISH] Started fishing')
             self._use_item()
         else:
-            self._terminal.log(f'[FISH] Stopped fishing')
+            self._terminal.console.log(f'[FISH] Stopped fishing')
             self._active = False
             self._use_item()
 
